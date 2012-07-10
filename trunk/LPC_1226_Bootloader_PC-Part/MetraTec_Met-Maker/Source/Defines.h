d2b95c5f6e0804c867382ace350ea519f6131c86
koehler4metratec@gmail.com <koehler4metratec@gmail.com@77844c6b-b198-b929-95e0-d00bf0a96383>
2012-07-10 11:34:47 +0000

    Changed sddresses to match new variable positioning in source file (.bin)

--- trunk/LPC_1226_Bootloader_PC-Part/MetraTec_Met-Maker/Source/Defines.h
+++ trunk/LPC_1226_Bootloader_PC-Part/MetraTec_Met-Maker/Source/Defines.h
@@ -11,16 +11,16 @@
 #define CMD_WRITE				0x02
 #define CMD_HANDSHAKE			0x01
 
-#define CODE_FW_NAME_Addr					0x00C0
+#define CODE_FW_NAME_Addr					0x0100
 #define CODE_FW_NAME_Length					16
-#define CODE_FW_Rev_Addr					0x00D0
+#define CODE_FW_Rev_Addr					0x0110
 #define CODE_FW_Rev_Length					4
-#define CODE_HW_NAME_Addr					0x00D4
+#define CODE_HW_NAME_Addr					0x0114
 #define CODE_HW_NAME_Length					16
-#define CODE_HW_Rev_Addr					0x00E4
+#define CODE_HW_Rev_Addr					0x0124
 #define CODE_HW_Rev_Length					4
-#define CODE_FW_LENGTH_IN_BYTE_ADDR			0x00E8
-#define CODE_FW_CRC_IN_MET					0x00EC
+#define CODE_FW_LENGTH_IN_BYTE_ADDR			0x0128
+#define CODE_FW_CRC_IN_MET					0x012C
 
 #define metFileNameSize (CODE_HW_NAME_Length+CODE_HW_Rev_Length+CODE_FW_NAME_Length+CODE_FW_Rev_Length+15)
 
